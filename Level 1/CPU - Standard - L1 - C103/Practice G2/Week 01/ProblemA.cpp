933f5e2582b12710dc73ff44f7c491d2b95acc98
Mohamed Rady <mrady2056@gmail.com>
2024-02-10 22:08:04 +0200

    Problem A

---
+++ Level 1/CPU - Standard - L1 - C103/Practice G2/Week 01/ProblemA.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+  int n , a , b;
+  cin >> n >> a >> b;
+  int ans = n - a + b;
+  cout << ans ;
+    return 0;
+}
--- Level 1/CPU - Standard - L1 - C103/Practice G2/Week 01/ProblemL.cpp
+++ Level 1/CPU - Standard - L1 - C103/Practice G2/Week 01/ProblemL.cpp
@@ -2,19 +2,6 @@
 using namespace std;
 int main() {
 
-    int n;
-    cin >> n; // number of dollars
-    int ans = 0; // minimum number if bills
-    ans += (n / 100); // number of 100
-    n  %= 100;
-    ans += (n/20);
-    n %= 20;
-    ans += (n/10);
-    n%=10;
-    ans += (n / 5);
-    n%=5;
-    ans += n ;
-    cout << ans ;
 
     return 0;
 }
