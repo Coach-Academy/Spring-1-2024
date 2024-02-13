b1998afcbb6bf500d10c065330df4ce1e5398792
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2024-02-13 03:04:53 +0200

    Create T - Round Down .cpp

---
+++ Level 2/CPU - Regular - L2 - C101/Practice/Week 01/T - Round Down .cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  char ch;
+  while (cin >> ch) {
+    if (ch == '.')break;
+    cout << ch;
+  }
+}
+
