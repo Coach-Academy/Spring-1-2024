40f9353e7e1a6d3dcedeaa69081a8527a5f63604
Mohamed Amir <m.elamir027@gmail.com>
2024-02-18 00:37:57 +0200

    Create A - UOIAUAI.cpp

---
+++ Level 1/CPU - Standard - L1 - C103/Practice G3/Week 02/A - UOIAUAI.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    if (c == 'a' || c == 'e' or c == 'i' or c == 'o' or c == 'u')
+        cout << "vowel\n";
+    else
+        cout << "consonant\n";
+}
