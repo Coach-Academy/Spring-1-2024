2bc1facf3c0b8fdd09f12e9cb48e2f201dbb2ef9
Rania Heragy <76109504+Rania154@users.noreply.github.com>
2024-02-17 21:30:15 +0200

    Add files via upload

---
+++ Level 1/CPU - Standard - L1 - C103/Lecture/Week 02/B.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b;
+   cin >> a >> b;
+   int x = a+b  ,y = a-b ,z = a*b;
+   if ( x >= y && x >= z) {
+       cout << x;
+   }
+   else if ( y >= x && y>= z)
+   {
+       cout << y;
+   }
+   else
+   {
+       cout << z ;
+   }
+   return 0;
+}
