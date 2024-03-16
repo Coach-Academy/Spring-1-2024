582ab67d78d08c5938ae98133478b2d5097bc301
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-03-16 22:28:39 +0200

    Create B - Counting Sort 2.cpp

---
+++ Level 2/CPU - Basic - L2 - C101/Practice/Week 05/B - Counting Sort 2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n;cin>>n;
+    int frq[100]={};
+    for(int i=0;i<n;i++)
+    {
+        int x;cin>>x;
+        frq[x]++;
+    }
+    for(int i=0;i<100;i++)
+    {
+         if(frq[i]>0)
+         {
+             while(frq[i]--)
+             {
+                 cout<<i<<' ';
+             }
+         }
+    }
+    return 0;
+}
