978ee29687e55ec4af77d36a34dfcc3a695c95aa
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-05-01 13:16:53 +0300

    Create N - Flipping bits.cpp

---
+++ Level 2/CPU - Basic - L2 - C101/Practice/Week 09/N - Flipping bits.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;cin>>t;while(t--)
+    {
+        unsigned int x;cin>>x;
+        cout<<~x<<'\n';
+    }
+    return 0;
+}
