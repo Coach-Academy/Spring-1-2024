d010d5c9f3b93bc7680f92127e08125637da4ca8
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-07-11 10:51:30 +0300

    Create E - Maximum GCD.cpp

---
+++ Level 2/CPU - Basic - L2 - C101/Practice/Week 11/E - Maximum GCD.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;cin>>t;while(t--)
+    {
+        int n;cin>>n;
+        cout<<n/2<<'\n';
+    }
+    return 0;
+}
