d72a6c826e2ae5f3af4b868b3a57ea9b14241dc4
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-08-07 20:56:02 +0300

    Create E - Prime Square.cpp

---
+++ Level 2/CPU - Regular - L2 - C101/Practice/Week 14/E - Prime Square.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll sod(ll n)
+{
+    ll ret=0;
+    while(n)
+    {
+        ret+=n%10;
+        n/=10;
+    }
+    return ret;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;cin>>t;while(t--)
+    {
+        int n;cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            for(int o=1;o<=n;o++)
+            {
+                if(i==o||o==i%n+1)
+                {
+                    cout<<1<<' ';
+                }
+                else
+                {
+                    cout<<0<<' ';
+                }
+            }
+            cout<<'\n';
+        }
+    }
+    return 0;
+}
