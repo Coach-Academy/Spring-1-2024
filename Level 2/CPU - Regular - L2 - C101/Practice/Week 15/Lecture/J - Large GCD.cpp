3d9fb7b0425c7a669944be4a1f8a9d212abccc10
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-08-07 21:08:17 +0300

    Create J - Large GCD.cpp

---
+++ Level 2/CPU - Regular - L2 - C101/Practice/Week 15/Lecture/J - Large GCD.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;cin>>t;while(t--)
+    {
+        int a,b;cin>>a>>b;
+        if((a+b)%2==0)
+        {
+            cout<<12<<'\n';
+        }
+        else
+        {
+            cout<<2<<'\n';
+        }
+    }
+    return 0;
+}
